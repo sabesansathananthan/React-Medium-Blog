ac869f7ab731758c7b9fb1cade96b00b412757a4
sabesansathananthan <sabesan.16@cse.mrt.ac.lk>
2019-12-12 21:43:08 +0530

    fix: unitTesting

--- src/utils/__test__/ToText.test.js
+++ src/utils/__test__/ToText.test.js
@@ -1,9 +1,9 @@
 import ToText from '../ToText';
 
 it('check whether ToText function is working or not', () => {
-  const a = '<div><P>This is a mock test for this function.</P></div>';
-  const b = 'This is a mock test for this function.';
+  const a = '<div>This is a mock test for this function</div>';
+  const b = 'This is a mock test for this function';
 
-  console.log(ToText(c));
-  // expect(ToText(a)).toBe(b);
+  // console.log(ToText(a));
+  expect(ToText(a)).toBe(b);
 });
